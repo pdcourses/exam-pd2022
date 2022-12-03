c028b3bab7c0caedf5a18866f0fdf7a2582070b3
pdcourses <freshitpt@gmail.com>
2022-12-03 11:00:26 +0200

    server js + app.js

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./router');
+const handlerError = require('./handlerError/handler');
+
+function createApp () {
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static('public'));
+app.use('/',router);
+app.use(handlerError);
+return app;
+
+}
+
+exports.createApp = createApp;
+
+express.app = createApp();
